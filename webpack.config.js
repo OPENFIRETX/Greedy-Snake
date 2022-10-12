10c742cbdd5009c8bb9a3c0f6b2006212f1841bc
tangxiao <1037706321@qq.com>
2022-10-12 23:27:45 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path")
+
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        environment: {
+            arrowFunction: false
+        },
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: ["babel-loader", "ts-loader"],
+                exclude: /node-modules/
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+                                            browsers: "last 2 version"
+                                        }
+                                    ]
+                                ]
+                            }
+                        }
+                    },
+                    "less-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            // title: "这是一个自定义的title"
+            template: "./src/index.html"
+        }),
+    ],
+
+}
